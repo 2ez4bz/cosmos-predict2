5462a53f640ce6c6c3441960903016d71492a2e4
Maciej Bala <mbala@nvidia.com>
2025-04-28 16:14:51 +0200

    bugfix

--- cosmos_predict1/diffusion/inference/video2world.py
+++ cosmos_predict1/diffusion/inference/video2world.py
@@ -38,7 +38,7 @@ def parse_arguments() -> argparse.Namespace:
         default="Cosmos-Predict1-2B-Video2World",
         help="DiT model weights directory name relative to checkpoint_dir",
         choices=[
-            "Cosmos-Predict1-2B-Video2World_post-trained",
+            "Cosmos-Predict1-2B-Video2World",
         ],
     )
     parser.add_argument(
